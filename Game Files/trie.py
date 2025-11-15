b1d37ad5655d8505891e3a262110f255ee456ab5
thatcherty <ryeking19@gmail.com>
2025-11-14 15:41:33 -0900

    Some space analysis when using dictionaries

--- Game Files/trie.py
+++ Game Files/trie.py
@@ -1,3 +1,4 @@
+import sys
 class trie:
     def __init__(self):
         self.three_in_a_row: dict[int, dict] = {}
@@ -19,12 +20,30 @@ class trie:
             level = level.setdefault(square, {})
         level[-1] = True # this is the leaf node
 
+    
+
 
 if __name__ == "__main__":
 
-    test_moves = [[5,6], [3,4], [7,8], [2,1]]
+    test1 = [[3, 2], [5, 1], [1, 0], [4, 4], [2, 5]]
+    test2 = [[0, 3], [3, 3], [1, 2], [5, 5], [4, 1], [2, 2]]
+    test3 = [[5, 0], [2, 3], [4, 2], [0, 1], [3, 4], [1, 0], [0, 0]]
+    test4 = [[1, 4], [2, 5], [0, 2], [4, 3], [3, 1]]
+    test5 = [[5, 5], [2, 1], [3, 0], [0, 5], [1, 3], [4, 4], [2, 2], [3, 3], [0, 0]]
+    test6 = [[1, 4], [2, 5], [0, 2], [4, 3], [3, 2]]
 
     test = trie()
 
-    test.add_seq(test_moves)
+    # size of dictionary without any items is 64 bytes
+    print(sys.getsizeof(test.three_in_a_row))
 
+    test.add_seq(test1)
+    test.add_seq(test2)
+    test.add_seq(test3)
+    test.add_seq(test4)
+    test.add_seq(test5)
+    test.add_seq(test6)
+
+    # size of dictionary with 4 levels is 224 bytes
+    # still 224 bytes with 9 levels
+    print(sys.getsizeof(test.three_in_a_row))
